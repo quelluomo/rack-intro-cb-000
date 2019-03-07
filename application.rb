a390c3cedcdcc9519f90b28708633ab01b84a1bc
Matteo Muscas <thatman.j@gmail.com>
2019-03-07 09:06:32 +0000

    Automatically backed up by Learn

--- application.rb
+++ application.rb
@@ -2,6 +2,12 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
+    resp.write "Hi! My name is"
+    resp.write "WHA"
+    resp.write "My name is"
+    resp.write "WHO"
+    resp.write "My name is"
+    resp.write "GNIKIGNIKI"
     resp.write "Herro, Warudo"
     resp.finish
   end
